b4f2f5efb025b9040e4c02947ce52d2088fe68ba
Nickname <megamen932@gmail.com>
2023-01-27 01:02:26 +0300

    v 1.382

--- bot/handlers/users/helpers.py
+++ bot/handlers/users/helpers.py
@@ -1,11 +1,12 @@
 from aiogram.types import Message
 
 from bot.keyboards.default import get_default_markup
+from bot.keyboards.default.default import get_instructions
 from loader import dp, _
 from models import User
 
 
 @dp.message_handler(state='*')
 async def _default_menu(message: Message, user: User):
-    await message.answer(_('Choose an action from the menu 👇'), reply_markup=get_default_markup(user))
+    await message.answer(get_instructions())
 
