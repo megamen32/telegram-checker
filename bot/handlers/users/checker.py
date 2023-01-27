995b809b6760444e1f8fa0b5b29370b5b05ece70
Nickname <megamen932@gmail.com>
2023-01-27 23:33:42 +0300

    auto commit after translation change

--- bot/handlers/users/checker.py
+++ bot/handlers/users/checker.py
@@ -143,17 +143,21 @@ async def render_text(analysys_completed, fake, more_than_month, one_three_days,
 <p><strong>ВСЕГО ПРОВЕРЕНО: {analysys_completed}</strong></p>
 <p>Подписчиков онлайн на момент проверки: <b>{online_count}</b></p>
 <p>Пользователь был онлайн: </p>  
-<li>до 3 дней назад: {one_three_days} ({one_three_days_p:.2f}%)</li>    
-<li>до 7 дней назад: {three_to_week} ({three_to_week_p:.2f}%)</li>    
-<li>до 30 дней назад: {week_to_month} ({week_to_month_p:.2f}%)</li>    
-<li>более 30 дней назад: {more_than_month} ({more_than_month_p:.2f}%) </li>  
+<ul> 
+    <li>до 3 дней назад: {one_three_days} ({one_three_days_p:.2f}%)</li>    
+    <li>до 7 дней назад: {three_to_week} ({three_to_week_p:.2f}%)</li>    
+    <li>до 30 дней назад: {week_to_month} ({week_to_month_p:.2f}%)</li>    
+    <li>более 30 дней назад: {more_than_month} ({more_than_month_p:.2f}%) </li>  
+</ul> 
 <p><b>Предположительная доля ботов</b> (заблокированные, удаленные, спам аккаунты): {fake} ({fake_percent:.2f}%)</p>   
 <p><b>Внимание!</b> Данный бот лишь один из методов проверки аудитории, не забывайте использовать полную статистику канала и дополнительные инструменты на сайте telemetr.me   </p>
 <p>____</p>
-<p>КАК ОПРЕДЕЛИТЬ НАКРУТКУ?</p>
-<ul><li>1.      </li>
-<li>2.      </li>
-<li>3.      </li>      </ul>
+<p><strong>КАК ОПРЕДЕЛИТЬ НАКРУТКУ?</strong></p>
+<ul>
+    <li>1.      </li>
+    <li>2.      </li>
+    <li>3.      </li> 
+</ul>
 <p>Больше статистических данных и полезной информации доступно на нашем сайте telemetr.me!</p>
     ''').format(analysys_completed=analysys_completed,real_people=real_people,real_percent= real_percent, fake=fake,fake_percent= 100 - real_percent,online_count=int(online_count),online_percent= online_count / analysys_completed * 100,
                 one_three_days=int(one_three_days), one_three_days_p=one_three_days / analysys_completed * 100,
