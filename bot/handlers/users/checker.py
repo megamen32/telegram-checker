806983f806c8c62ad2d93e6e49b0254367de599a
Nickname <megamen932@gmail.com>
2023-02-04 18:34:54 +0300

    auto commit after translation change

--- bot/handlers/users/checker.py
+++ bot/handlers/users/checker.py
@@ -44,9 +44,9 @@ async def analys_start(message: Message, user: User):
         if db_ch is None:
             month=-1
             while month<0.01:
-                one_three = max(0, random.gauss(0.6, 0.1))
-                three_week =max(0, random.gauss(0.2, 0.1))
-                week_month = max(0,random.gauss(0.1, 0.05))
+                one_three = max(0.0, random.gauss(0.6, 0.1))
+                three_week =max(0.0, random.gauss(0.2, 0.1))
+                week_month = max(0.0,random.gauss(0.1, 0.05))
                 month = 1 - one_three - three_week - week_month
                 if min(one_three,week_month,month,three_week)<0.01:continue
             db_ch = Channel.create(name=channel, not_fake_percent=current_count, followers_count=followers_count, online_percent=random.gauss(0.05,0.01), recent_percent=one_three,
